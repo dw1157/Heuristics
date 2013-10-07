661f124596641fcf86570a55a8ebea76176c676f
Danilo Ribeiro <dnr2@cin.ufpe.br>
2013-10-07 13:31:16 -0300

    initial ant colonization algorithm

--- src/Ambulances_ADD.java
+++ src/Ambulances_ADD.java
@@ -1,18 +1,24 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Ambulances_ADD {
 
   private static final int NUM_KMEANS_TRIALS = 10000;
-
-  private static int[][] victims = new int[300][3];
+  private static final int ANTCOL_ITERATIONS = 100;
+  private static final int MAX_NUMBER_PERSONS = 300;
+  
+  private static int[][] victims = new int[MAX_NUMBER_PERSONS][3];
   private static int[] hospitals_start = new int[5];
-  private static int[][] hospitals;
+  private static int[][] nearstHospitals = new int[MAX_NUMBER_PERSONS][2];
+  \private static int[][] hospitals;
   private static int max_x = 100;
   private static int max_y = 100;
-
+  private static int numOfPersons = 0;
+  
+  
   public static void main(String[] args) throws FileNotFoundException {
 
     readFile();
@@ -89,12 +95,51 @@ public class Ambulances_ADD {
       }
     }
 
-    //System.out.println("Best score: " + best_score);
-
-    //printMap();
+    System.out.println("Best score: " + best_score);
+    
+    runAnt();
+    
+    printMap();
 
   }
-
+  
+  static class Edge{
+	  int x, y; // origin
+	  int z, w; // destination
+	  public boolean equals(Object o ){
+		  if( ! ( o instanceof Edge )) return false;
+		  Edge p = (Edge) o;
+		  return ( x == p.x && y == p.y && z == p.z && w == p.w );
+	  }
+  }
+  
+  private static void runAnt(){
+	
+	int bestScore = 0;
+	Random random = new Random();
+	HashMap<Edge, Integer> graph = new HashMap<Edge,Integer>();
+	int[] numPreviousSaves = new int[numOfPersons]; 
+	
+	for (int i = 0; i < ANTCOL_ITERATIONS; i++) {
+		int x = hospitals[random.nextInt(5)][0];
+		int y = hospitals[random.nextInt(5)][1];
+		boolean[] saved = new boolean[numOfPersons];
+		int time = 0;
+		for (int j = 0; j < numOfPersons; j++) {
+			int[] nearstH = nearstHospitals[j];
+			int dist = abs(x-victims[j][0]) +  abs(y-victims[j][1]) +  abs(nearstH[0]-victims[j][0]) +  abs(nearstH[1]-victims[j][1]);
+			if( !saved[j] && ( time +  dist + 2) >= victims[j][2] ){
+				
+			}
+		}
+	}
+	  
+  }
+  
+  static int abs(int x){
+	  return Math.abs(x);
+  }
+  
   private static double distance(int[] point1, int[] point2) {
 
     return Math.sqrt((point1[0] - point2[0]) * (point1[0] - point2[0])
@@ -108,21 +153,21 @@ public class Ambulances_ADD {
     scan.nextLine();
 
     String line;
-    int i = 0;
+    int numOfPersons = 0;
     while (scan.hasNext() && !(line = scan.nextLine()).isEmpty()) {
       String[] pers_coord = line.split(",");
-      victims[i][0] = Integer.parseInt(pers_coord[0]);
-      victims[i][1] = Integer.parseInt(pers_coord[1]);
-      victims[i][2] = Integer.parseInt(pers_coord[2]);
+      victims[numOfPersons][0] = Integer.parseInt(pers_coord[0]);
+      victims[numOfPersons][1] = Integer.parseInt(pers_coord[1]);
+      victims[numOfPersons][2] = Integer.parseInt(pers_coord[2]);
       // if (max_x < victims[i][0]) max_x = victims[i][0];
       // if (max_y < victims[i][1]) max_y = victims[i][1];
-      i++;
+      numOfPersons++; 
     }
 
     while ((line = scan.nextLine()).isEmpty()) {
 
     }
-    i = 0;
+    int i = 0;
     while (scan.hasNextLine()) {
       line = scan.nextLine();
       hospitals_start[i] = Integer.parseInt(line);
